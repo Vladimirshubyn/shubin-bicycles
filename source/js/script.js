29492ef35feff07f57e804ef79d1d9b8da17200e
Vladimirshubyn <vladimirshubyn@gmail.com>
2021-07-05 18:05:07 +0700

    реализовал js для меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,19 @@
+"use strict";
 
+const nav = document.querySelector('#nav');
+const navtoggle = document.querySelector('#nav-toggle');
+
+const initializeNav = () => {
+  nav.classList.add('main-nav--js');
+  navtoggle.classList.remove('visually-hidden');
+}
+
+initializeNav();
+
+const toggleNav = () => {
+  nav.classList.toggle('main-nav--js');
+  nav.classList.toggle('main-nav--absolute');
+  navtoggle.classList.toggle('header__toggle--open');
+}
+
+navtoggle.addEventListener('click', toggleNav);
