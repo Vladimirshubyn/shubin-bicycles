5ab6c68fad2a2377830b55e4d12f5415eb06d687
Vladimirshubyn <vladimirshubyn@gmail.com>
2021-07-06 16:45:56 +0700

    начал реализацию валидации формы на js, правки по критериям

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,15 @@
 "use strict";
 
 const nav = document.querySelector('#nav');
-const navtoggle = document.querySelector('#nav-toggle');
+const navToggle = document.querySelector('#nav-toggle');
+const page = document.querySelector('.page');
+const form = document.querySelector('#form');
+const phoneInput = document.querySelector('input[name="phone"]');
+const submitButton = document.querySelector('#form-submit');
 
 const initializeNav = () => {
   nav.classList.add('main-nav--js');
-  navtoggle.classList.remove('visually-hidden');
+  navToggle.classList.remove('visually-hidden');
 }
 
 initializeNav();
@@ -13,7 +17,24 @@ initializeNav();
 const toggleNav = () => {
   nav.classList.toggle('main-nav--js');
   nav.classList.toggle('main-nav--absolute');
-  navtoggle.classList.toggle('header__toggle--open');
+  navToggle.classList.toggle('header__toggle--open');
+  page.classList.toggle('page--menu-open');
 }
 
-navtoggle.addEventListener('click', toggleNav);
+const formSubmit = (evt) => {
+  evt.preventDefault();
+}
+
+const validateInput = (evt) => {
+  const array = phoneInput.value.split(' ');
+  phoneInput.setCustomValidity('');
+
+  if (phoneInput.value = '/[^\d]/g/') {
+    evt.target.setCustomValidity('Нужно вводить только цифры');
+    return;
+  }
+}
+
+navToggle.addEventListener('click', toggleNav);
+form.addEventListener('submit', formSubmit);
+submitButton.addEventListener('click', validateInput);
