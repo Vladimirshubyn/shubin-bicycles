020a80050c923baa2c33de9a2962c610475bb509
Vladimirshubyn <vladimirshubyn@gmail.com>
2021-07-14 16:39:22 +0700

    исправления по замечаниям наставника 2

--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,36 @@
-import './form.js';
-import './menu.js';
+'use strict';
+
+(function init() {
+  var nav = document.querySelector('#nav');
+  var navToggle = document.querySelector('#nav-toggle');
+  var page = document.querySelector('.page');
+  var phoneInput = document.querySelector('input[name="phone"]');
+  var submitButton = document.querySelector('#form-submit');
+
+  var initializeNav = function () {
+    nav.classList.add('main-nav--js');
+    navToggle.classList.remove('visually-hidden');
+  };
+
+  initializeNav();
+
+  var toggleNav = function () {
+    nav.classList.toggle('main-nav--js');
+    nav.classList.toggle('main-nav--absolute');
+    navToggle.classList.toggle('header__toggle--open');
+    page.classList.toggle('page--menu-open');
+  };
+
+  var validateInput = function () {
+    var lettersPattern = /[0-9]/;
+
+    if (!phoneInput.value.match(lettersPattern)) {
+      phoneInput.setCustomValidity('Нужно вводить только цифры');
+    } else {
+      phoneInput.setCustomValidity('');
+    }
+  };
+
+  navToggle.addEventListener('click', toggleNav);
+  submitButton.addEventListener('click', validateInput);
+}());
